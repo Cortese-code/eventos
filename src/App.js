0114c842d5f6c391bb29dbe71a18e81dcbce7cbd
Cortese-code <albertodiegocortese@gmail.com>
2022-07-16 17:41:43 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import imagen0 from './images/rey_atanagildo.png';
+import imagen1 from './images/rey_ataulfo.png';
+import {useRef} from 'react';
 import './App.css';
 
 function App() {
+  const cambio=23.16;
+  const refCaja=useRef();
+  function incrementar(e){
+    e.target.innerHTML=Number(e.target.innerHTML)+1;
+    if(e.target.innerHTML>=10){
+      e.target.innerHTML=1
+    }
+    if(e.target.innerHTML>=8){
+      e.target.style.backgroundColor='red';
+    }else{
+      e.target.style.backgroundColor='white';
+    }
+  }
+
+  function convertir(){
+    refCaja.current.innerHTML=Number(refCaja.current.innerHTML)*cambio;
+
+  }
+
+  function cambiar(e){
+    if(e.target.src.includes('atanagildo')){
+      e.target.src=imagen1;
+    }else{
+      e.target.src=imagen0;
+    }
+    
+  }
+
+  const lectura=(e)=>{
+    refCaja.current.innerHTML=e.target.value;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div ref={refCaja} className="caja" onClick={incrementar}>1</div>
+      <button onClick={convertir}>Aceptar</button>
+       <div><img onClick={cambiar} src={imagen0}/></div>
+      <input onChange={lectura} className="campo"/>
+    </>
   );
 }
 
